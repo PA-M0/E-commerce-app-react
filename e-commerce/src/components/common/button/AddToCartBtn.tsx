5b6307becc8a4dead0414e5e69ba1531421e2dcf
ParsaShafagh <parsa.shafagh.1382@gmail.com>
2024-07-06 19:33:01 +0330

    :white_check_mark: button is added.

---
+++ e-commerce/src/components/common/button/AddToCartBtn.tsx
@@ -0,0 +1,9 @@
+import react from 'react'
+import styles from './AddToCartBtn.module.scss'
+
+export const AddToCartBtn = () =>{
+
+     <button className={styles.button}>Remove From Basket</button>
+    
+}
+export default AddToCartBtn;
