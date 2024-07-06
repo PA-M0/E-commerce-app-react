321c0aa80b8fb3743416e71ca293b0e3fa257c19
ParsaShafagh <parsa.shafagh.1382@gmail.com>
2024-07-06 19:34:16 +0330

    :white_check_mark: product page is added.
    specific page for every products

--- e-commerce/src/components/common/product/Product.tsx
+++ e-commerce/src/components/common/product/Product.tsx
@@ -1,23 +1,26 @@
+//PoductCard
 import React from 'react';
 import { ProductShape as ProductType } from '../types/product';
 import styles from './Product.module.scss';
-
+import { Link } from 'react-router-dom'
 interface ProductProps {
   product: ProductType;
 }
 
-const Product = ({ product }:ProductProps) => {
- 
+const Product = ({ product }: ProductProps) => {
+
   return (
     <div className={styles.product}>
-    <div className={styles.productImageBackground}>
-      <img src={product.image} alt={product.title} className={styles.productImage} />
+      <Link to={`/product/${product.id}`}>
+        <div className={styles.productImageBackground}>
+          <img src={product.image} alt={product.title} className={styles.productImage} />
+        </div>
+        <h2 className={styles.productName}>{product.title}</h2>
+        <p className={styles.productDescription}>{product.description}</p>
+        <p className={styles.productPrice}>${product.price}</p>
+        <button className={styles.addToCartButton}>Add to Cart</button>
+      </Link>
     </div>
-    <h2 className={styles.productName}>{product.title}</h2>
-    <p className={styles.productDescription}>{product.description}</p>
-    <p className={styles.productPrice}>${product.price}</p>
-    <button className={styles.addToCartButton}>Add to Cart</button>
-  </div>
   );
 };
 
