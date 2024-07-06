5e15af8671d49ce94822f01f83baad648f2d8332
ParsaShafagh <parsa.shafagh.1382@gmail.com>
2024-07-06 19:40:20 +0330

    :white_check_mark: added getSpececifice Product function for fetching a product

--- e-commerce/src/services/ProductService.ts
+++ e-commerce/src/services/ProductService.ts
@@ -3,9 +3,19 @@ import axios from 'axios';
 
 
 
-export const getProducts = async (): Promise<ProductShape[]> => {
+export const getProducts = async (API_URL:string): Promise<ProductShape[]> => {
   try {
-    const response = await axios.get('https://fakestoreapi.com/products');
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    throw new Error('Network response was not ok');
+  }
+};
+
+
+export const getSpecificeProduct = async (API_URL:string): Promise<ProductShape> => {
+  try {
+    const response = await axios.get(API_URL);
     return response.data;
   } catch (error) {
     throw new Error('Network response was not ok');
