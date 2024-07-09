34e7d9aecdef68d4f178b51fae910d01ee8d6af6
ParsaShafagh <parsa.shafagh.1382@gmail.com>
2024-07-10 02:41:07 +0330

    reformat code

--- e-commerce/src/components/common/cart/Cart.tsx
+++ e-commerce/src/components/common/cart/Cart.tsx
@@ -10,7 +10,9 @@ export interface CartProps {
 }
 
 const Cart: React.FC = () => {
-  const [cart, setCart] = useLocalStorageState<CartProps>("cart", { defaultValue: {} });
+  const [cart, setCart] = useLocalStorageState<CartProps>("cart", {
+    defaultValue: {},
+  });
 
   const getTotalPrice = () => {
     return Object.values(cart || {})
@@ -49,7 +51,11 @@ const Cart: React.FC = () => {
       <div className={styles.header}>
         <h1 className={styles.title}>My Basket</h1>
         <div className={styles.actions}>
-          <Button buttonType="removeFromCart"  onClick={removeAllFromCart} label="Clear Basket" />
+          <Button
+            buttonType="removeFromCart"
+            onClick={removeAllFromCart}
+            label="Clear Basket"
+          />
         </div>
       </div>
       {Object.keys(cart || {}).length === 0 ? (
@@ -62,31 +68,55 @@ const Cart: React.FC = () => {
             {Object.values(cart || {}).map((product) => (
               <li key={product.id} className={styles.cartItem}>
                 <div className={styles.productInfo}>
-                  <img src={product.image} width={50} height={50} alt={product.title} className={styles.productImage} />
+                  <img
+                    src={product.image}
+                    width={50}
+                    height={50}
+                    alt={product.title}
+                    className={styles.productImage}
+                  />
                   <div className={styles.productDetails}>
                     <h2 className={styles.productTitle}>{product.title}</h2>
-                    <div className={styles.productAttributes}>
-                      <span>Quantity: {product.count}</span>
-                      <span>Size: {product.size}</span>
-                      <span>Color: {product.color}</span>
-                    </div>
+                    <div className={styles.productAttributes}></div>
                     <div className={styles.productCount}>
-                      <Button label="-" onClick={() => updateProductCount(product.id, product.count - 1)} />
+                      <Button
+                        label="-"
+                        buttonType="removeFromCart"
+                        onClick={() =>
+                          updateProductCount(product.id, product.count - 1)
+                        }
+                      />
                       <span>{product.count}</span>
-                      <Button label="+" onClick={() => updateProductCount(product.id, product.count + 1)} />
+                      <Button
+                        label="+"
+                        buttonType="removeFromCart"
+                        onClick={() =>
+                          updateProductCount(product.id, product.count + 1)
+                        }
+                      />
                     </div>
-                    <Button label="X" onClick={() => removeFromCart(product.id)} className={styles.removeButton} />
                   </div>
-                  <div className={styles.productPrice}>${(product.price * product.count).toFixed(2)}</div>
+                  <div className={styles.productPrice}>
+                    ${(product.price * product.count).toFixed(2)}
+                    <Button
+                    label="X"
+                    onClick={() => removeFromCart(product.id)}
+                    buttonType="removeFromCart"
+                  />
+                  </div>
+                  
+
+                 
                 </div>
               </li>
             ))}
           </ul>
           <div className={styles.total}>
             <h2>Subtotal Amount: ${getTotalPrice()}</h2>
+        
           </div>
           <div className={styles.checkout}>
-            <Button label="CHECK OUT" className={styles.checkoutButton} />
+            <Button label="CHECK OUT" buttonType="addToCart" />
           </div>
         </div>
       )}
