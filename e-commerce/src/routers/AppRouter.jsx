0e618e8337b0b2e33161475661b8d6dbf5ef1458
ParsaShafagh <parsa.shafagh.1382@gmail.com>
2024-07-10 04:57:17 +0330

    :fire: delete unnecessery code

--- e-commerce/src/routers/AppRouter.jsx
+++ e-commerce/src/routers/AppRouter.jsx
@@ -1,12 +1,12 @@
 import react from "react";
 import Footer from "../components/common/footer/Footer.jsx";
-import Header from "../components/common/header/Header.jsx";
+import Header from "../components/common/header/Header.tsx";
 
 // import * as ROUTES from '@/constants/routes';
 // import { createBrowserHistory } from 'history';
 import { Route, Routes } from "react-router-dom";
 // import * as view from '@/views';
-import Home from "../view/Home";
+import Home from "../view/Home.tsx";
 import Shop from "../view/Shop";
 import About from "../view/About";
 import Product from "../view/Product.tsx";
@@ -20,44 +20,11 @@ const AppRouter = () => (
   {/* <ProductsContext.Provider value="products"> */}
     <Header />
     <Routes>
-      {/* <Navigation /> */}
-
-      {/* <Switch>
-        <Route
-          component={view.Search}
-          exact
-          path={ROUTES.SEARCH}
-        />
-        <Route
-          component={view.Home}
-          exact
-          path={ROUTES.HOME}
-        />
-        <Route
-          component={view.Shop}
-          exact
-          path={ROUTES.SHOP}
-        />
-        <Route
-          component={view.FeaturedProducts}
-          exact
-          path={ROUTES.FEATURED_PRODUCTS}
-        />
-        <Route
-          component={view.RecommendedProducts}
-          exact
-          path={ROUTES.RECOMMENDED_PRODUCTS}
-        />
-       
-        <PublicRoute component={view.PageNotFound} />
-      </Switch> */}
-
       <Route path="/" element={<Home />} />
       <Route path="/shop" element={<Shop />} />
       <Route path="/about" element={<About />} />
       <Route path="/product/:id" element={<Product />} />
       <Route path="/manage" element={<ProductManager />} />
-
     </Routes>
     <Footer />
     {/* </ProductsContext.Provider> */}
